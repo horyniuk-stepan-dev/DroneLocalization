468d15b09d24a6aa1b7ba043ad5f40fabdf94d50
darkstozhar <horyniukstepan@gmail.com>
2026-07-05 19:51:33 +0200

    fable test

--- scratch/check_cuda.py
+++
@@ -1,7 +0,0 @@
-import torch
-if torch.cuda.is_available():
-    prop = torch.cuda.get_device_properties(0)
-    print(f"Device: {prop.name}")
-    print(f"Compute Capability: {prop.major}.{prop.minor}")
-else:
-    print("CUDA not available")
